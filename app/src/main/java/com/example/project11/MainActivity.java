8685c92f60655707b9bb6f2caaedc38e65e3fdd8
admin <dinhthanh197@gmail.com>
2019-10-27 21:32:22 +0700

    update project tuần mới.

--- app/src/main/java/com/example/project11/MainActivity.java
+++ app/src/main/java/com/example/project11/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.project11;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -18,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<ThongTin> arrayList2;
     RecyclerView recyclerViewthang,recyclerView;
     TextView textnam;
+    ImageView imgtheme,btnthemchude, btntaichude;;
     ThongTinAdapter thongTinBeAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +47,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         textnam = (TextView) findViewById(R.id.text_nam);
         textnam.setText(thoigianngay());
+        btnthemchude = (ImageView) findViewById(R.id.btn_them_chu_de);
+        btntaichude = (ImageView) findViewById(R.id.btn_tai_chu_de);
+        btntaichude.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialogtaichude();
+            }
+        });
+        btnthemchude.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialogthemchude();
+            }
+        });
         addthang();
         recyclerthang();
         addthongtinthangnay();
@@ -71,6 +104,60 @@ public class MainActivity extends AppCompatActivity {
         arrayList2.add(new ThongTin( "chu de","noi dung", thoigian()));
         arrayList2.add(new ThongTin( "chu de","noi dung", thoigian()));
         arrayList2.add(new ThongTin( "chu de","noi dung", thoigian()));
+    }
+    private void dialogthemchude() {
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.dialog_themchude);
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.getWindow().setLayout(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        final ImageButton imgintentcamera, imgdowloatcamera;
+        final EditText edttenchude;
+        Button btnyes, btnno;
+
+        imgintentcamera = (ImageButton) dialog.findViewById(R.id.img_btn_camera);
+        imgdowloatcamera = (ImageButton) dialog.findViewById(R.id.img_btn_dowloat);
+        edttenchude = (EditText) dialog.findViewById(R.id.edt_name_theme);
+        btnno = (Button) dialog.findViewById(R.id.btn_no);
+        btnyes = (Button) dialog.findViewById(R.id.btn_yes);
+        imgtheme = (ImageView) dialog.findViewById(R.id.img_theme);
+        imgintentcamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        imgdowloatcamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+        btnyes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+        btnno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.cancel();
+            }
+        });
+        dialog.show();
+    }
+    private void dialogtaichude(){
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.dialog_taianh);
+        LinearLayout linearLayout1;
+        final ImageView imageView1;
+        linearLayout1 = (LinearLayout) dialog.findViewById(R.id.layout1);
+        imageView1 = (ImageView) dialog.findViewById(R.id.imageView_box);
+        linearLayout1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        dialog.show();
     }
     public String thoigianngay() {
         Calendar calendar = Calendar.getInstance();
